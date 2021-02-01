138c6817f5d9b3cfdc206ef9097a27c6be7c3c31
NathanHarbour <nathanharbour@gmail.com>
2021-01-31 23:11:52 -0600

    Take break jobs only when breaker healthy as well

--- Source/CM_Beat_Prisoners/WorkGiver_Warden_Break.cs
+++ Source/CM_Beat_Prisoners/WorkGiver_Warden_Break.cs
@@ -20,10 +20,11 @@ namespace CM_Beat_Prisoners
             BeatingTracker beatingTracker = Current.Game.World.GetComponent<BeatingTracker>();
 
             bool canGiveBeating = (beatingTracker?.CanGiveBeating(pawn) ?? true) && pawn.health.capacities.CapableOf(PawnCapacityDefOf.Manipulation);
+            bool initiatorHealty = Mathf.Abs(1.0f - (pawn.health?.summaryHealth?.SummaryHealthPercent ?? 1.0f)) <= 0.01f;
             bool targetHealthy = Mathf.Abs(1.0f - (pawn2.health?.summaryHealth?.SummaryHealthPercent ?? 1.0f)) <= 0.01f;
             bool prisonerCanReceiveBeating = (interactionMode == PrisonerInteractionModeDefOf.ReduceResistance && !pawn2.Downed);
             
-            if (canGiveBeating && prisonerCanReceiveBeating && (targetHealthy || beatingTracker.GetBeatingInProgress(pawn2) != null))
+            if (canGiveBeating && prisonerCanReceiveBeating && initiatorHealty && (targetHealthy || beatingTracker.GetBeatingInProgress(pawn2) != null))
             {
                 Job breakJob = JobMaker.MakeJob(BeatPrisonersDefOf.CM_Beat_Prisoners_Job_Break_Resistance, target);
                 breakJob.maxNumMeleeAttacks = Rand.RangeInclusive(3, 9);
